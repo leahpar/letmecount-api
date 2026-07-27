3b5987bb6c3a5e9879ba68e7a7eae749021c308b
Raphaël Bacco <raphael@bacco.fr>
2026-07-27 15:31:32 +0200

    Retours divers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,23 +7,40 @@ use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController
 {
     #[Route('/auth/{token}', name: 'auth')]
     public function auth(
+        Request $request,
         EntityManagerInterface $em,
         string $token,
         JWTTokenManagerInterface $JWTManager,
-        AuthenticationSuccessHandler $authenticationSuccessHandler
+        AuthenticationSuccessHandler $authenticationSuccessHandler,
+        RateLimiterFactoryInterface $authCodeLimiter
     ): Response
     {
+        // Le code ne fait que 6 chiffres : on limite le bruteforce par IP
+        $limit = $authCodeLimiter->create($request->getClientIp())->consume();
+        if (!$limit->isAccepted()) {
+            throw new TooManyRequestsHttpException(
+                $limit->getRetryAfter()->getTimestamp() - time(),
+                'Trop de tentatives, réessayez plus tard'
+            );
+        }
+
         $user = $em->getRepository(User::class)->findOneBy(['token' => $token]);
         if (!$user) {
             throw $this->createAccessDeniedException('Token invalide');
         }
+
+        // Code valide : on rend ses tentatives à l'utilisateur
+        $authCodeLimiter->create($request->getClientIp())->reset();
 
         $JWTManager->create($user);
 
